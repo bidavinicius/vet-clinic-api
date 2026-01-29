6a0117bca350d323d4b4ab9f1a27204c438b793d
bidavinicius <bidaviniciusribas@gmail.com>
2026-01-29 03:58:43 -0300

    feat: add CRUD logic for veterinarians

---
+++ src/controllers/vetController.js
@@ -0,0 +1,32 @@
+import db from "../database/db.js";
+
+//Essa função estava fora do requerido no teste, mas como é uma função básica de CRUD, achei interessante incluir.
+export const listarVet = async (req, res) => {
+  try {
+    return res.json(db.veterinarios);
+  } catch (error) {
+    return res.status(500).json({ error: "Erro no sistema ao listar veterinários." });
+  }
+};
+
+export const cadastrarVet = async (req, res) => {
+  try {
+    const { nome } = req.body;
+
+    if (!nome) {
+      return res
+        .status(400)
+        .json({ error: "Nome do veterinário é obrigatório." });
+    }
+    const newVet = {
+      id: db.idVet,
+      nome,
+    };
+
+    db.veterinarios.push(newVet);
+    db.idVet++;
+    return res.status(201).json(newVet);
+  } catch (error) {
+    return res.status(500).json({ error: "Erro no sistema ao criar veterinário." });
+  }
+};
