a903ccf97798147ef620246046c9bd94b5058e85
bidavinicius <bidaviniciusribas@gmail.com>
2026-01-29 09:48:43 -0300

    feat: add scheduling logic for appointments

---
+++ src/controllers/consultaController.js
@@ -0,0 +1,42 @@
+import db from "../database/db.js";
+
+export const listarConsulta = async (req, res) => {
+    try {
+        return res.json(db.consultas);
+    } catch (error) {
+        return res.status(500).json({ error: "Erro no sistema ao listar consultas." });
+    }
+}
+
+export const cadastrarConsulta = async (req, res) => {
+    try {
+        const { dataConsulta, idAnimal, idVeterinario, motivo, observacoes } = req.body;
+
+        if (!dataConsulta || !idAnimal || !idVeterinario || !motivo) {
+            return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+        }
+
+        const animalExiste = db.animais.find(animal => animal.id == idAnimal);
+        const vetExiste = db.veterinarios.find(vet => vet.id == idVeterinario);
+
+        if (!animalExiste || !vetExiste) {
+            return res.status(400).json({ erro: 'Animal ou Veterinário inválido' });
+        }
+
+        const novaConsulta = {
+            id: db.idConsulta,
+            dataConsulta,
+            idAnimal,
+            idVeterinario,
+            motivo,
+            observacoes
+        };
+
+        db.consultas.push(novaConsulta);
+        db.idConsulta++;
+        return res.status(201).json(novaConsulta);
+    } catch (error) {
+        return res.status(500).json({ error: "Erro no sistema ao criar consulta." });
+    }
+}
+
