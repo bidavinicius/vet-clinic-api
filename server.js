38e35944a335b9d16e140b641b00729feddad149
bidavinicius <bidaviniciusribas@gmail.com>
2026-01-29 10:54:06 -0300

    feat: ensure database connection is established before app startup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import app from './src/app.js';
+import connectDB from './src/database/db.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
 });
